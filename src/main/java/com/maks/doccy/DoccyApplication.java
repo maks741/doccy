5193e2dd06ee6d48927125e151bc0c79f5ba17c0
Maksym Kozak <bersercer70@gmail.com>
2025-01-31 21:32:39 +0200

    initial commit

---
+++ src/main/java/com/maks/doccy/DoccyApplication.java
@@ -0,0 +1,13 @@
+package com.maks.doccy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoccyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoccyApplication.class, args);
+	}
+
+}
